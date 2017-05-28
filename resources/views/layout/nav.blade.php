ffcfc378e7dd656445d442ba206d3bf6ffe19ab9
alex <contialessandro@hotmail.it>
2017-05-28 12:52:25 +0100

    hide features not allowed for not admins

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -3,9 +3,9 @@
     <div class="container">
         <nav class="nav blog-nav">
             <a class="nav-link active" href="/">Home</a>
-            <a class="nav-link" href="/login">Login</a>
-            <a class="nav-link" href="/register">Register</a>
-            <a class="nav-link" href="/posts/create">create</a>
+            @if(!Auth::check())
+                <a class="nav-link" href="/login">Login</a>
+            @endif
             @if(Auth::check())
                 <a class="pull-right nav-link" href="/logout">Logout {{ Auth::user()->name }}</a>
             @endif
